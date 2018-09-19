2571c4b9357b98008d42d7716532bc7e085e1e77
Anson Foong <afoong@albany.edu>
2018-09-19 04:57:43 -0400

    Wrote WordModifier.java to remove spaces up to a non-space char
    Implemented a WordModifier class with external methods to help my code. removeSpaces method takes in the pathname, and removes spaces that lead up to the first character of the path.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import ext.anson.*;
 public class Main {
 	public static void main(String[] args)
 	{
@@ -8,23 +9,29 @@ public class Main {
 		boolean running = true;
 		while(running)
 		{
-			commandInput = key.nextLine();
+			
+			commandInput = key.nextLine().trim();
+			
 			if(commandInput.equals("q") || commandInput.equals("quit"))
 				running = false;
 			
 			else if(commandInput.startsWith("ls"))
 			{
-				String command = new String(commandInput.substring(0, 3));
-				String path = new String(commandInput.substring(3));
-				System.out.println(command);
-				System.out.println(path);
+				if(commandInput.equals("ls"))
+				{
+					shell.listDirectoryFiles();
+				}
+				else
+				{
+					String command = new String(commandInput.substring(0, 2));
+					String path = new String(commandInput.substring(3));
+					path = WordModifier.removeSpaces(path);
+					System.out.println(path);
+				}
 			}
 			
 		}
 		
 	}
-	public static void removeSpaces(String str)
-	{
-		
-	}
+	
 }
---
+++ src/ext/anson/WordModifier.java
@@ -0,0 +1,22 @@
+package ext.anson;
+
+public class WordModifier {
+	public static String removeSpaces(String str)
+	{
+		String newPath = "";
+		int i = 0;
+		while(i<str.length())
+		{
+			if(str.charAt(i) == ' ' || str.charAt(i) == '\t')
+				i++;
+			else
+			{
+				newPath = str.substring(i);
+				return newPath;
+				// i-th subscript.
+				//      CSI\ 201
+			}
+		}
+		return newPath;
+	}
+}
